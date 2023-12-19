8becd8ae34d77466723ba96dd93f5eb81d66d431
Radu <radu.bobanga14@gmail.com>
2023-12-19 19:23:51 +0200

    First commit

---
+++ src/Tema1/Cerinta8.java
@@ -0,0 +1,18 @@
+package Tema1;
+import java.util.Scanner;
+
+public class Cerinta8 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
